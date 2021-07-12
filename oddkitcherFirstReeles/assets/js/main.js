9220ded01b9ba551e2dbc2257abf8e59d9c112b0
acasax <acasax@gmail.com>
2021-07-13 01:09:43 +0200

    start location

--- oddkitcherFirstReeles/assets/js/main.js
+++ oddkitcherFirstReeles/assets/js/main.js
@@ -7,6 +7,19 @@ $(document).ready(function() {
         pre_loader.fadeOut(3000, function() { $(this).remove(); });
     });
 
+    /*skuter */
+
+    document.getElementById('skuter').addEventListener('ended', myHandler, false);
+
+    setTimeout(() => { console.log("World!"); }, 10000);
+
+    function myHandler(e) {
+        console.log('ended');
+
+        setTimeout(function() {
+            document.getElementById('skuter').play();
+        }, 3000);
+    }
 
 
 });
