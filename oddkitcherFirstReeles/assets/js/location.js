e46e6b941cfd1487fc0383479c3be6e194d53fe4
aca sax <acasax@gmail.com>
2021-08-14 00:59:56 +0200

    finish locations

--- oddkitcherFirstReeles/assets/js/location.js
+++ oddkitcherFirstReeles/assets/js/location.js
@@ -14,6 +14,8 @@ $(document).ready(function() {
         setTimeout(function() {
             document.getElementById('skuter').playbackRate = 2.0;
             document.getElementById('skuter').play();
+            document.getElementById('skuter1').playbackRate = 2.0;
+            document.getElementById('skuter1').play();
         }, 5000);
 
 
