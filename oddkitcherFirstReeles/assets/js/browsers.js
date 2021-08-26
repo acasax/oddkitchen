4a5981fa8e5f7776777929025fadcc4293283c8e
acasax <acasax@gmail.com>
2021-08-26 15:27:59 +0200

    fix phone gif and image

---
+++ oddkitcherFirstReeles/assets/js/browsers.js
@@ -0,0 +1,22 @@
+var result = detect.parse(navigator.userAgent);
+console.log(result);
+/*document.write("You are using " + result.browser.family +
+    " v" + result.browser.version +
+    " on " + result.os.family); */
+
+var pcVideo = document.getElementById("videoPC");
+var mobVideo = document.getElementById("videoMOB");
+
+if (result.device.family === "iPhone" || result.device.family === "iPad" || result.device.family === "iPod") {
+    pcVideo.innerHTML = '<img src="assets/img/home/1024x515.gif" alt="" width="100%"></img>';
+    mobVideo.innerHTML = '<img src="assets/img/home/1024x515.gif" alt="" width="100%"></img>';
+    console.log(result.os.family);
+} else {
+    pcVideo.innerHTML = '<video autoplay muted playsinline width="100%">' +
+        '<source src="assets/img/GOTOVO_VP9_2pass.webm" type="video/webm">' +
+        '</video>';
+    mobVideo.innerHTML = '<video autoplay muted playsinline width="100%">' +
+        '<source src="assets/img/home/5124r_VP9_2pass.webm">' +
+        '</video>';
+    console.log(result.os.family);
+}
